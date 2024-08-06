4f6b3762af6b28f5bfb67083130bd50b368c13b3
Avishek-kr <ak251997@gmail.com>
2024-08-06 22:47:29 +0530

    mongoDB added

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
+import { configDotenv } from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+configDotenv();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
